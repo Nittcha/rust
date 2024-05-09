879c405b4a147175e750d0f26e1ee27a90d2015d
Praeploy Mongkhonsaen <nmongkolsaen@gmail.com>
2024-05-09 16:52:32 +0700

    update

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+fn main() {
+   
+//    let num : i32= 3;
+
+//     if num > 5 {
+//         println!("more than 5")
+//     } else if num > 4 {
+//         println!("more than 4")
+//     } else if num == 3 {
+//         println!("more than 3")
+//     }else {
+//         println!("less than 3")
+//     }
+
+       // A > 80
+       // B [70, 80]
+       // C [60, 70]
+       // D [50, 60]
+       // E < 50
+
+       let score: i32 = 65;
+        
+        if score > 80 {
+            println!("A");
+        } else if score >= 70 {
+            println!("B");
+        } else if score >= 60 {
+            println!("C");
+        } else if score >= 50 {
+            println!("D")
+        } else {
+            println!("E")
+        }
+
+
+}
+
+
